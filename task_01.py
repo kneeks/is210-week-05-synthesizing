2967821aae47413828889765af23cf2539723339
Vagrant User <mr.nikovaccaro@gmail.com>
2015-09-28 03:45:14 +0000

    added function to give cur. date

---
+++ task_01.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module gives you the current date"""
+
+
+# Import sys libs
+import datetime
+
+CURDATE = None
+
+def get_current_date():
+    """
+    This function gives you the current date.
+
+    Args:
+        date (int): the current date.
+
+    Returns:
+        int: returns date in number format
+
+    Examples:
+
+        >>> task_01.get_current_date()
+        datetime.date(2015, 1, 1)
+
+    """
+    date = datetime.date.today()
+    return date
